d100a2c9014c45592d6e179364380fdf93f80dab
Jesse <jvanrheenen@gmail.com>
2018-04-02 15:34:12 +0200

    Get Chuck Jokes
    Fetch the chuck norris jokes from url

--- src/App.js
+++ src/App.js
@@ -3,16 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      data: []
+    };
+  };
+
+  componentDidMount() {
+    fetch("http://api.icndb.com/jokes/random/10")
+    .then( (response) => {
+      return response.json() })
+    .then( (json) => {
+      this.setState({data: json.value});
+      console.log(this.state.data);
+    })
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Chuck Norris Case</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <ul>
+            {
+              this.state.data.map(function(item) {
+                return (
+                  <div className="Employee">
+                    <li> {item.joke} </li>
+                  </div>
+                )
+              })
+            }
+          </ul>
+        </div>
       </div>
     );
   }
